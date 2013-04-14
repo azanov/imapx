94fa6bb4399977f3384c6b4b11e2cf10b30a766d
pavel_azanov_cp <SND\pavel_azanov_cp>
2013-04-14 23:03:49 +0000

    - Added OAuth2 support - Updated the sample application to provide authentication for GMail through OAuth2

---
+++ ImapX.Sample/Google/GoogleProfile.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ImapX.Sample.Google
+{
+    public class GoogleProfile
+    {
+        public string id { get; set; }
+        public string email { get; set; }
+    }
+}
