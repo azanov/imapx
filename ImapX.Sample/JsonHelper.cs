a53a821926947b202324d7995eb081759d81b439
pavel_azanov_cp <SND\pavel_azanov_cp>
2013-08-21 09:32:22 +0000

    - Included fix for issue 1701 - Minor code refactoring

--- ImapX.Sample/JsonHelper.cs
+++ ImapX.Sample/JsonHelper.cs
@@ -1,7 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
+using System.Runtime.Serialization.Json;
 using System.Text;
 
 namespace ImapX.Sample
@@ -10,27 +9,25 @@ namespace ImapX.Sample
     {
         public static string To<T>(T obj)
         {
-            System.Runtime.Serialization.Json.DataContractJsonSerializer serializer = new System.Runtime.Serialization.Json.DataContractJsonSerializer(obj.GetType());
-            using (MemoryStream ms = new MemoryStream())
+            var serializer = new DataContractJsonSerializer(obj.GetType());
+            using (var ms = new MemoryStream())
             {
                 serializer.WriteObject(ms, obj);
                 string retVal = Encoding.Default.GetString(ms.ToArray());
                 return retVal;
             }
-            
         }
 
         public static T From<T>(string json)
         {
-            T obj = Activator.CreateInstance<T>();
-            using (MemoryStream ms = new MemoryStream(Encoding.Unicode.GetBytes(json)))
+            var obj = Activator.CreateInstance<T>();
+            using (var ms = new MemoryStream(Encoding.Unicode.GetBytes(json)))
             {
-                System.Runtime.Serialization.Json.DataContractJsonSerializer serializer = new System.Runtime.Serialization.Json.DataContractJsonSerializer(obj.GetType());
-                obj = (T)serializer.ReadObject(ms);
+                var serializer = new DataContractJsonSerializer(obj.GetType());
+                obj = (T) serializer.ReadObject(ms);
             }
 
             return obj;
         }
-
     }
 }
