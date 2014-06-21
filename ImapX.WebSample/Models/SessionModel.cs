f53c8c0bb5fb954d26586de2b2061719316f2d30
Pavel Azanov <info@wyto.net>
2014-06-21 10:33:05 +0000

    - Started developing of an MVC 4 Sample application

---
+++ ImapX.WebSample/Models/SessionModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.SessionState;
+
+namespace ImapX.WebSample.Models
+{
+    public class SessionModel
+    {
+
+        public ImapClient Client
+        {
+            get
+            {
+                return Get<ImapClient>("ImapClient");
+            }
+            set
+            {
+                Set("ImapClient", value);
+            }
+        }
+
+        #region Session management
+
+        public static HttpSessionState Current
+        {
+            get
+            {
+                return HttpContext.Current.Session;
+            }
+        }
+
+        public static void Set(string key, dynamic value)
+        {
+            Current[key] = value;
+        }
+
+        public static T Get<T>(string key)
+        {
+            return (T)(Current[key] ?? default(T));
+        }
+
+        #endregion
+
+    }
+}
